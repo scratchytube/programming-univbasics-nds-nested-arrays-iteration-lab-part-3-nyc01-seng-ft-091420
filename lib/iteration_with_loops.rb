e5caff607fc71b6a517baa90aff0c59b7b0440ef
Brian Sahota <bpsahota@gmail.com>
2020-09-07 19:56:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -12,16 +12,14 @@ end
 
 
 
-
-
-# final_strings = " "
+# final_strings = []
 #   row_index = 0 
 #   while row_index < src.count do 
 #     element_index = 0
 #   while element_index < src[row_index].count do 
 #     if src[row_index][element_index].class == String 
 #       phrase = src[row_index][element_index]
-    
+    phrase << words
     
 #     end
 #       element_index += 1
